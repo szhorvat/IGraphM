7f567818faaaf907a647ae5a3396efe12c4f3492
Szabolcs Horvát <szhorvat@gmail.com>
2015-09-16 00:10:55 +0200

    version 0.1.2

--- IGraphM/IGraphM.m
+++ IGraphM/IGraphM.m
@@ -6,7 +6,7 @@
 (* :Author: szhorvat  *)
 (* :Date: 2015-08-28  *)
 
-(* :Package Version: 0.1.2dev *)
+(* :Package Version: 0.1.2 *)
 (* :Mathematica Version: 10.0 *)
 (* :Copyright: (c) 2015 Szabolcs Horvát *)
 (* :Keywords: igraph, graphs, networks, LibraryLink *)
@@ -194,7 +194,7 @@ If[Not@OrderedQ[{10.0, 2}, {$VersionNumber, $ReleaseNumber}],
 
 (***** Package variables *****)
 
-$packageVersion    = "0.1.2dev";
+$packageVersion    = "0.1.2";
 $packageDirectory  = DirectoryName[$InputFileName];
 $libraryDirectory  = FileNameJoin[{$packageDirectory, "LibraryResources", $SystemID}];
 $sourceDirectory   = FileNameJoin[{$packageDirectory, "LibraryResources", "Source"}];
