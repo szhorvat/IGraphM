9a2866a5eeb118e914b200db26e641f9cd12d54b
Szabolcs Horvát <szhorvat@gmail.com>
2016-11-22 16:25:59 +0100

    add PacletInfo.m

---
+++ IGraphM/PacletInfo.m
@@ -0,0 +1,16 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+Paclet[
+    Name -> "IGraphM",
+    Version -> "0.3.0",
+    MathematicaVersion -> "10.0+",
+    Description -> "igraph interface for Mathematica.",
+    Creator -> "Szabolcs Horvát",
+    Extensions -> 
+        {    
+            {"Kernel", Root -> ".", Context -> "IGraphM`"},
+            {"LibraryLink"}
+        }
+]
