25c6b72bbc87cf827c2679ae965ca0c29afb11ff
Szabolcs Horvát <szhorvat@gmail.com>
2017-04-07 17:11:33 +0200

    Update PacletInfo

--- IGraphM/PacletInfo.m
+++ IGraphM/PacletInfo.m
@@ -4,12 +4,13 @@
 
 Paclet[
     Name -> "IGraphM",
-    Version -> "0.2.92",
+    Version -> "0.2.93",
     MathematicaVersion -> "10.0+",
     Description -> "IGraph/M \[Dash] the igraph interface for Mathematica.",
     Creator -> "Szabolcs Horvát <szhorvat@gmail.com>",
-    URL -> "https://github.com/szhorvat/IGraphM",
-    SystemID -> {"MacOSX-x86-64", "Windows-x86-64", "Linux-x86-64"},
+    URL -> "http://szhorvat.net/mathematica/IGraphM",
+    Thumbnail -> "Logo.png",
+    SystemID -> {"MacOSX-x86-64", "Windows-x86-64", "Linux-x86-64", "Linux-ARM"},
     Extensions -> 
         {    
             {"Kernel", Root -> ".", Context -> "IGraphM`"},
