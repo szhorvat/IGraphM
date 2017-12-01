7b557ebf856c30ed5ee0b70dd477cb3d1fd553d4
Szabolcs Horvát <szhorvat@gmail.com>
2017-12-01 19:45:11 +0100

    PacletInfo: version

--- IGraphM/PacletInfo.m
+++ IGraphM/PacletInfo.m
@@ -4,7 +4,7 @@
 
 Paclet[
     Name -> "IGraphM",
-    Version -> "0.3.93",
+    Version -> "0.3.93.1",
     MathematicaVersion -> "10.0+",
     Description -> "IGraph/M \[Dash] the igraph interface for Mathematica.",
     Creator -> "Szabolcs Horvát <szhorvat@gmail.com>",
