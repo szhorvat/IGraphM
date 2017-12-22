62ca4a2128aee79493c913b75b4ebee946862a77
Szabolcs Horvát <szhorvat@gmail.com>
2017-12-22 21:59:15 +0100

    init.m: fix auto-completion and allow easy reloading

--- IGraphM/Kernel/init.m
+++ IGraphM/Kernel/init.m
@@ -1,2 +1,7 @@
 
+Get["IGraphM`Utilities`"] (* Also loaded in BeginPackage in IGraphM.m. The purpose of this line is to easy re-loading during development. *)
 Get["IGraphM`IGraphM`"]
+
+(* Workaround for Utilities auto-completion getting disabled *)
+BeginPackage["IGraphM`Utilities`"]
+EndPackage[]
