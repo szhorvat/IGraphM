fe264eaf4c6783aa650d6f19d57f27c7fe4cf565
Szabolcs Horvát <szhorvat@gmail.com>
2018-12-01 14:35:16 +0100

    Fix BuildSettings again

--- IGraphM/BuildSettings.m
+++ IGraphM/BuildSettings.m
@@ -18,7 +18,7 @@ Switch[$OperatingSystem,
 
     (* IGraphM requires C++11. With OS X's default compiler this is supported 10.9 and up only,
        thus we need to override the default -mmacosx-version-min=10.6 option. *)
-    "CompileOptions" -> {"-mmacosx-version-min=10.9", "-fsanitize=address", " -fsanitize-recover=address", "-fno-omit-frame-pointer"},
+    "CompileOptions" -> {"-mmacosx-version-min=10.9", "-flto=thin"},
 
     (* Statically link the igraph library *)
     "ExtraObjectFiles" -> {"$HOME/local/lib/libigraph.a", "$HOME/local/lib/libgmp.a", "$HOME/local/lib/libglpk.a", "$HOME/local/lib/libemon.a"},
