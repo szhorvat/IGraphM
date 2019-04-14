903c559b025e135d7d859b1210bd91e9a836f631
Szabolcs Horvát <szhorvat@gmail.com>
2019-04-14 22:15:08 +0200

    BuildSettings: Remove -mmacosx-version-min because LTemplate takes care of this.

--- IGraphM/BuildSettings.m
+++ IGraphM/BuildSettings.m
@@ -17,8 +17,9 @@ Switch[$OperatingSystem,
     "SystemCompileOptions" -> "-O3 -m64 -fPIC -framework Foundation -framework mathlink",*)
 
     (* IGraphM requires C++11. With OS X's default compiler this is supported 10.9 and up only,
-       thus we need to override the default -mmacosx-version-min=10.6 option. *)
-    "CompileOptions" -> {"-mmacosx-version-min=10.9", "-flto=thin"},
+       thus we need to override the default -mmacosx-version-min=10.6 option. LTemplate now
+       does this automatically. *)
+    "CompileOptions" -> {"-flto=thin"},
 
     (* Statically link the igraph library *)
     "ExtraObjectFiles" -> {"$HOME/local/lib/libigraph.a", "$HOME/local/lib/libgmp.a", "$HOME/local/lib/libglpk.a", "$HOME/local/lib/libemon.a"},
