9c2e0f3d47a422962bad95ab98a18aca5f912a4d
Szabolcs Horvát <szhorvat@gmail.com>
2020-03-22 14:21:11 +0100

    IGAverageNeighbourDegree and IGDegreeConnectivity: move to different section of source file

--- IGraphM/Miscellaneous.m
+++ IGraphM/Miscellaneous.m
@@ -143,6 +143,38 @@ IGImmediateDominators[graph_?igGraphQ, root_] :=
     ]
 
 
+(***** Neighbour degrees and assortativity *****)
+
+PackageExport["IGAverageNeighborDegree"]
+IGAverageNeighborDegree::usage =
+    "IGAverageNeighborDegree[graph] gives the average neighbour degree of the vertices of graph.\n" <>
+    "IGAverageNeighborDegree[graph, {vertex1, vertex2, \[Ellipsis]}] gives the average neighbour degree of the specified vertices.\n" <>
+    "IGAverageNeighborDegree[graph, All, mode] uses the given mode, \"In\", \"Out\" or \"All\", to find neighbours and degrees in directed graphs. The default is \"Out\".\n" <>
+    "IGAverageNeighborDegree[graph, All, degreeMode, neighborMode] uses different modes for finding neighbours and degrees.";
+
+SyntaxInformation[IGAverageNeighborDegree] = {"ArgumentsPattern" -> {_, _., _., _.}};
+IGAverageNeighborDegree[graph_?igGraphQ, {}, degMode_String, neiMode_String] := {}
+IGAverageNeighborDegree[graph_?igGraphQ, vs : (_List | All) : All, degMode_String : "Out", neiMode : _String | Automatic : Automatic] :=
+    catch@Block[{ig = igMakeFastWeighted[graph]},
+      check@ig@"averageNeighborDegree"[vss[graph][vs], encodeNeighborMode@Replace[neiMode, Automatic -> degMode], encodeNeighborMode[degMode]]
+    ]
+addCompletion[IGAverageNeighborDegree, {0, 0, {"In", "Out", "All"}, {"In", "Out", "All"}}]
+
+
+PackageExport["IGAverageDegreeConnectivity"]
+IGAverageDegreeConnectivity::usage =
+    "IGAverageDegreeConnectivity[graph] gives the average neighbour degree for vertices of degree k=1, 2, \[Ellipsis]\n" <>
+    "IGAverageDegreeConnectivity[graph, mode] uses the given mode, \"In\", \"Out\" or \"All\", to find neighbours and degrees in directed graphs. The default is \"Out\".\n" <>
+    "IGAverageDegreeConnectivity[graph, degreeMode, neighborMode] uses different modes for finding neighbours and degrees.";
+
+SyntaxInformation[IGAverageDegreeConnectivity] = {"ArgumentsPattern" -> {_, _., _.}};
+IGAverageDegreeConnectivity[graph_?igGraphQ, degMode_String : "Out", neiMode : _String | Automatic : Automatic] :=
+    catch@Block[{ig = igMakeFastWeighted[graph]},
+      check@ig@"averageDegreeConnectivity"[encodeNeighborMode@Replace[neiMode, Automatic -> degMode], encodeNeighborMode[degMode]]
+    ]
+addCompletion[IGAverageDegreeConnectivity, {0, {"In", "Out", "All"}, {"In", "Out", "All"}}]
+
+
 (***** Other functions *****)
 
 PackageExport["IGTreelikeComponents"]
@@ -175,31 +207,3 @@ IGCactusQ[graph_?UndirectedGraphQ] :=
 IGCactusQ::dirg = "IGCactusQ is not implemented for directed graphs.";
 IGCactusQ[_?DirectedGraphQ] := (Message[IGCactusQ::dirg]; $Failed)
 IGCactusQ[_] := False
-
-
-PackageExport["IGAverageNeighborDegree"]
-IGAverageNeighborDegree::usage =
-    "IGAverageNeighborDegree[graph] gives the average neighbour degree of the vertices of graph.\n" <>
-    "IGAverageNeighborDegree[graph, {vertex1, vertex2, \[Ellipsis]}] gives the average neighbour degree of the specified vertices.\n" <>
-    "IGAverageNeighborDegree[graph, All, mode] uses the given mode, \"In\", \"Out\" or \"All\", to find neighbours and degrees in directed graphs. The default is \"Out\".\n" <>
-    "IGAverageNeighborDegree[graph, All, degreeMode, neighborMode] uses different modes for finding neighbours and degrees.";
-SyntaxInformation[IGAverageNeighborDegree] = {"ArgumentsPattern" -> {_, _., _.}};
-IGAverageNeighborDegree[graph_?igGraphQ, {}, degMode_, neiMode_] := {}
-IGAverageNeighborDegree[graph_?igGraphQ, vs : (_List | All) : All, degMode_ : "Out", neiMode_ : Automatic] :=
-    catch@Module[{ig = igMakeFastWeighted[graph], nMode = If[neiMode === Automatic, degMode, neiMode]},
-      check@ig@"averageNeighborDegree"[vss[graph][vs], encodeNeighborMode[nMode], encodeNeighborMode[degMode]]
-    ]
-addCompletion[IGAverageNeighborDegree, {0, 0, {"In", "Out", "All"}, {"In", "Out", "All"}}]
-
-
-PackageExport["IGAverageDegreeConnectivity"]
-IGAverageDegreeConnectivity::usage =
-    "IGAverageDegreeConnectivity[graph] gives the average neighbour degree for vertices of degree k=1, 2, \[Ellipsis]\n" <>
-    "IGAverageDegreeConnectivity[graph, mode] uses the given mode, \"In\", \"Out\" or \"All\", to find neighbours and degrees in directed graphs. The default is \"Out\".\n" <>
-    "IGAverageDegreeConnectivity[graph, degreeMode, neighborMode] uses different modes for finding neighbours and degrees.";
-SyntaxInformation[IGAverageDegreeConnectivity] = {"ArgumentsPattern" -> {_, _., _.}};
-IGAverageDegreeConnectivity[graph_?igGraphQ, degMode_ : "Out", neiMode_ : Automatic] :=
-    catch@Block[{ig = igMakeFastWeighted[graph], nMode =If[neiMode === Automatic, degMode, neiMode] },
-      check@ig@"averageDegreeConnectivity"[encodeNeighborMode[nMode], encodeNeighborMode[degMode]]
-    ]
-addCompletion[IGAverageDegreeConnectivity, {0, {"In", "Out", "All"}, {"In", "Out", "All"}}]
