8f40f4c52c7ef910eb3db1099df5bb70ad26c15e
Szabolcs Horvát <szhorvat@gmail.com>
2021-01-29 15:54:33 +0100

    Update macOS build settings

--- IGraphM/BuildSettings.m
+++ IGraphM/BuildSettings.m
@@ -16,13 +16,15 @@ Switch[$OperatingSystem,
     "CompilerName" -> "clang++-mp-7.0",
     "SystemCompileOptions" -> "-O3 -m64 -fPIC -framework Foundation -framework mathlink",*)
 
-    (* IGraphM requires C++11. With OS X's default compiler this is supported 10.9 and up only,
-       thus we need to override the default -mmacosx-version-min=10.6 option. LTemplate now
-       does this automatically. *)
-    "CompileOptions" -> {"-flto=thin" (* "-g -O0 -fno-omit-frame-pointer -fsanitize=address" *)},
+    "CompileOptions" -> {
+      "-flto=thin", "-O3",
+      (*"-g -Og -fno-omit-frame-pointer -fsanitize=address -fsanitize=undefined", *)
+      "-framework Accelerate", (* for BLAS and LAPACK *)
+      "-mmacosx-version-min=10.9", (* earliest supported macOS version---required for C++11 *)
+      "-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk" },
 
     (* Statically link the igraph library *)
-    "ExtraObjectFiles" -> {"$HOME/local/lib/libigraph.a", "$HOME/local/lib/libgmp.a", "$HOME/local/lib/libglpk.a", "$HOME/local/lib/libemon.a"},
+    "ExtraObjectFiles" -> {"$HOME/local/lib/libigraph.a", "$HOME/local/lib/libglpk.a", "$HOME/local/lib/libemon.a"},
 
     (* Set igraph location *)
     "IncludeDirectories" -> {"$HOME/local/include"},
